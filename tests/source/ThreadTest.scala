b2f06ab19dfe087faab2451fb2a52d206d2a7c05
Robert <rob@lidalia.org.uk>
2015-11-23 08:17:02 +0000

    Experimenting with error handling

--- tests/source/ThreadTest.scala
+++ tests/source/ThreadTest.scala
@@ -1,8 +1,5 @@
-import java.lang.Thread.UncaughtExceptionHandler
-import java.util
-
-import org.slf4j.{Logger, LoggerFactory}
 import uk.org.lidalia.exampleapp.system.logging.LogbackLoggingDefinition
+import uk.org.lidalia.scalalang.ChildThread
 
 object ThreadTest {
 
@@ -24,28 +21,4 @@ object ThreadTest {
 }
 
 
-class ChildThread extends Thread {
 
-  var parentStack: Option[Array[StackTraceElement]] = None
-
-  setUncaughtExceptionHandler(new UncaughtExceptionHandler {
-    override def uncaughtException(t: Thread, e: Throwable): Unit = {
-      val fullStack = Array.concat(e.getStackTrace, t.asInstanceOf[ChildThread].parentStack.get)
-      e.setStackTrace(fullStack)
-      LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME).error("Uncaught exception in thread {}", t, e: Any)
-    }
-  })
-
-  override def start(): Unit = {
-    val trace = Thread.currentThread().getStackTrace
-    parentStack = Some(util.Arrays.copyOfRange(trace, 2, trace.length))
-    super.start()
-  }
-
-  override def getStackTrace: Array[StackTraceElement] = {
-    parentStack match {
-      case None => super.getStackTrace
-      case Some(parentElements) => Array.concat(super.getStackTrace, parentElements)
-    }
-  }
-}

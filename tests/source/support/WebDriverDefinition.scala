7322c4964cfdcfc1cd9eb91a72fac2ab289ad314
Robert <rob@lidalia.org.uk>
2015-12-06 23:59:02 +0000

    Logging around web driver startup

--- tests/source/support/WebDriverDefinition.scala
+++ tests/source/support/WebDriverDefinition.scala
@@ -4,15 +4,19 @@ import java.io.File
 
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeDriverService.Builder
+import org.slf4j.LoggerFactory
+import uk.org.lidalia.exampleapp.system.HasLogger
 import uk.org.lidalia.scalalang.ResourceFactory
 import uk.org.lidalia.scalalang.ResourceFactory._try
 
 object WebDriverDefinition {
 
   def apply() = new WebDriverDefinition
+
+  val log = LoggerFactory.getLogger(classOf[WebDriverDefinition])
 }
 
-class WebDriverDefinition private() extends ResourceFactory[ReusableWebDriver] {
+class WebDriverDefinition private() extends ResourceFactory[ReusableWebDriver] with HasLogger {
 
   override def using[T](work: (ReusableWebDriver) => T): T = {
 
@@ -20,10 +24,14 @@ class WebDriverDefinition private() extends ResourceFactory[ReusableWebDriver] {
     val chromeDriverService = new Builder().usingDriverExecutable(driverFile).build()
 
     _try {
+      log.info("Starting web driver")
       chromeDriverService.start()
+      log.info("Started web driver")
       work(ReusableWebDriver(new ChromeDriver(chromeDriverService)))
     } _finally {
+      log.info("Stopping web driver")
       chromeDriverService.stop()
+      log.info("Stopped web driver")
     }
   }
 

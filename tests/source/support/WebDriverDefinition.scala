90a51008e9123ad7dcf4725d00924c9e6f7d3127
Robert Elliot <rob@lidalia.org.uk>
2015-12-07 09:02:23 +0000

    Attempt at efficiency - the driver file shouldn't change in the life of the JVM

--- tests/source/support/WebDriverDefinition.scala
+++ tests/source/support/WebDriverDefinition.scala
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeDriverService.Builder
 import org.slf4j.LoggerFactory
 import uk.org.lidalia.exampleapp.system.HasLogger
+import uk.org.lidalia.exampleapp.tests.support.WebDriverDefinition.driverFile
 import uk.org.lidalia.scalalang.ResourceFactory
 import uk.org.lidalia.scalalang.ResourceFactory._try
 
@@ -14,30 +15,32 @@ object WebDriverDefinition {
   def apply() = new WebDriverDefinition
 
   val log = LoggerFactory.getLogger(classOf[WebDriverDefinition])
+
+  lazy val driverFile: File = {
+    val driverUrl = Thread.currentThread().getContextClassLoader.getResource("chromedriver-mac")
+    new File(driverUrl.toURI)
+  }
 }
 
 class WebDriverDefinition private() extends ResourceFactory[ReusableWebDriver] with HasLogger {
 
   override def using[T](work: (ReusableWebDriver) => T): T = {
 
-    val driverFile: File = findChromeDriver
     val chromeDriverService = new Builder().usingDriverExecutable(driverFile).build()
 
     _try {
       log.info("Starting web driver")
       chromeDriverService.start()
+
       val driver = ReusableWebDriver(new ChromeDriver(chromeDriverService))
       log.info("Started web driver")
+
       work(driver)
+
     } _finally {
       log.info("Stopping web driver")
       chromeDriverService.stop()
       log.info("Stopped web driver")
     }
-  }
-
-  def findChromeDriver: File = {
-    val driverUrl = Thread.currentThread().getContextClassLoader.getResource("chromedriver-mac")
-    new File(driverUrl.toURI)
   }
 }

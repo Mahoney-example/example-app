e59aeb33b73f436a11fca8a473080d4952671c0c
Robert Elliot <rob@lidalia.org.uk>
2016-04-26 19:55:14 +0100

    Added logback support

--- tests/source/support/OnePerRun.scala
+++ tests/source/support/OnePerRun.scala
@@ -2,8 +2,10 @@ package uk.org.lidalia.exampleapp.tests.support
 
 import java.util.concurrent.ConcurrentHashMap
 
+import ch.qos.logback.classic.Level.INFO
 import org.scalatest._
 import uk.org.lidalia.exampleapp.local.{Environment, EnvironmentDefinition}
+import uk.org.lidalia.exampleapp.system.logging.{LogbackLoggerFactory, LogbackLoggingDefinition}
 import uk.org.lidalia.scalalang.{PoolFactory, ResourceFactory}
 
 import scala.collection.JavaConverters._
@@ -30,21 +32,34 @@ trait WebDriverDefinitionPerRun extends OnePerRun {
   }
 }
 
+trait LogbackLoggerFactoryPerRun extends OnePerRun {
+
+  private val key = "LoggerContext"
+
+  register(key, LogbackLoggingDefinition(
+    "uk.org.lidalia" -> INFO
+  ))
+
+  def loggerContext(configMap: ConfigMap) = {
+    getResourceFactory[LogbackLoggerFactory](configMap, key)
+  }
+}
+
 trait OnePerRun extends Suite {
 
-  private val factoryRegister = new ConcurrentHashMap[String, ResourceFactory[_]]().asScala
+  private val factories = new ConcurrentHashMap[String, ResourceFactory[_]]().asScala
 
-  def register(key: String, factory: ResourceFactory[_]) = factoryRegister.putIfAbsent(key, factory)
+  def register(key: String, factory: ResourceFactory[_]) = factories.putIfAbsent(key, factory)
 
   override def run(testName: Option[String], args: Args): Status = {
 
-    if (args.configMap.contains("PerRunResources")) {
-      super.run(testName, args)
-    } else {
-      ResourceFactory.usingAll(factoryRegister.values.toSeq:_*) { resources =>
-        val appendedConfigMap = args.configMap.+("PerRunResources" -> ConfigMap(factoryRegister.keys.zip(resources).toSeq:_*))
-        super.run(testName, args.copy(configMap = appendedConfigMap))
-      }
+    val existing = args.configMap.getWithDefault[ConfigMap]("PerRunResources", ConfigMap())
+    val toCreate = factories.filter { pair => !existing.contains(pair._1) }
+    ResourceFactory.usingAll(toCreate.values.toSeq:_*) { resources =>
+      val created = ConfigMap(factories.keys.zip(resources).toSeq: _*)
+      val all = existing.++(created)
+      val appendedConfigMap = args.configMap.+("PerRunResources" -> all)
+      super.run(testName, args.copy(configMap = appendedConfigMap))
     }
   }
 
@@ -53,9 +68,14 @@ trait OnePerRun extends Suite {
   }
 }
 
-class SomeTests extends org.scalatest.fixture.FunSuite with EnvironmentDefinitionPerRun with WebDriverDefinitionPerRun {
+class SomeTests extends org.scalatest.fixture.FunSuite with EnvironmentDefinitionPerRun with WebDriverDefinitionPerRun with LogbackLoggerFactoryPerRun {
 
   test("blah") { case (env, driver) =>
+    println(env)
+    println(driver)
+  }
+
+  test("blah2") { case (env, driver) =>
     println(env)
     println(driver)
   }
@@ -67,3 +87,25 @@ class SomeTests extends org.scalatest.fixture.FunSuite with EnvironmentDefinitio
     }
   }
 }
+
+class SomeOtherTests extends org.scalatest.fixture.FunSuite with EnvironmentDefinitionPerRun {
+  test("blah") { env =>
+    println(env)
+  }
+
+  test("blah2") { env =>
+    println(env)
+  }
+  override type FixtureParam = Environment
+
+  override protected def withFixture(test: OneArgTest): Outcome = {
+    environmentDefinition(test.configMap).using { env =>
+      test.apply(env)
+    }
+  }
+}
+
+class SomeSuite extends Suites(
+  new SomeTests,
+  new SomeOtherTests
+) with EnvironmentDefinitionPerRun with WebDriverDefinitionPerRun with LogbackLoggerFactoryPerRun

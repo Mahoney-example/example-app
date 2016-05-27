84bca60fe3c3d6087ae5db7dad0464bb15cd2ce5
Robert Elliot <rob@lidalia.org.uk>
2016-05-27 12:53:45 +0100

    Pooled resources work in suites without needing a main method

--- tests/source/FunctionalTestSuites.scala
+++ tests/source/FunctionalTestSuites.scala
@@ -1,18 +1,31 @@
 package uk.org.lidalia
 package exampleapp.tests
 
-import org.scalatest.{ParallelTestExecution, Suites}
-import uk.org.lidalia.exampleapp.local.Environment
-import uk.org.lidalia.exampleapp.tests.library.ReusableWebDriver
-import uk.org.lidalia.exampleapp.tests.support.BrowserFunctionalTestsRunner
-import uk.org.lidalia.scalalang.ResourceFactory
+import ch.qos.logback.classic.Level.INFO
+import org.scalatest.{Args, Status, Suite, Suites}
+import uk.org.lidalia.exampleapp.local.{Environment, EnvironmentDefinition}
+import uk.org.lidalia.exampleapp.system.logging.LogbackLoggingDefinition
+import uk.org.lidalia.exampleapp.tests.library.{ReusableWebDriver, WebDriverDefinition}
+import uk.org.lidalia.scalalang.{PoolFactory, ResourceFactory}
 
-class FunctionalTestSuites(
-  envFactory: ResourceFactory[Environment],
-  webDriverFactory: ResourceFactory[ReusableWebDriver]
-) extends Suites(
-   new LoginTests(envFactory, webDriverFactory),
-   new RegisterTests(envFactory, webDriverFactory)
-) with ParallelTestExecution
+class FunctionalTestSuites extends Suite {
 
-object FunctionalTestSuites extends BrowserFunctionalTestsRunner(new FunctionalTestSuites(_, _))
+  def suites(
+    envFactory: ResourceFactory[Environment],
+    webDriverFactory: ResourceFactory[ReusableWebDriver]
+  ) = List(
+    new LoginTests(envFactory, webDriverFactory),
+    new RegisterTests(envFactory, webDriverFactory)
+  )
+
+  override def run(testName: Option[String], args: Args): Status = {
+    LogbackLoggingDefinition("uk.org.lidalia" -> INFO).using { loggerFactory =>
+      ResourceFactory.usingAll(
+        PoolFactory(EnvironmentDefinition(loggerFactory = loggerFactory)),
+        PoolFactory(WebDriverDefinition())
+      ) { (envFactory, webDriverFactory) =>
+        new Suites(suites(envFactory, webDriverFactory):_*).run(testName, args)
+      }
+    }
+  }
+}

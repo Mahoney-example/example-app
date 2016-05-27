6d86dc72ddc43a06671247f0ce6b3bcf7b7d48a5
Robert Elliot <rob@lidalia.org.uk>
2016-05-27 14:55:35 +0100

    Code formatting

--- tests/source/FunctionalTestSuites.scala
+++ tests/source/FunctionalTestSuites.scala
@@ -9,17 +9,22 @@ import uk.org.lidalia.scalalang.ResourceFactory
 
 class FunctionalTestSuites extends Suite {
 
-  def suites(
+  private def suites(
     envFactory: ResourceFactory[Environment],
     webDriverFactory: ResourceFactory[ReusableWebDriver]
   ) = List(
+
     new LoginTests(envFactory, webDriverFactory),
     new RegisterTests(envFactory, webDriverFactory)
   )
 
   override def run(testName: Option[String], args: Args): Status = {
+
     FunctionalTestEnvironment().using { factories =>
-      new Suites(suites(factories._1, factories._2):_*).run(testName, args)
+
+      new Suites(
+        suites(factories._1, factories._2): _*
+      ).run(testName, args)
     }
   }
 }
